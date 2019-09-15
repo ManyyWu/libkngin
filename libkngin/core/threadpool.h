8235ba818fb47f0f2bcab542b2797452d5424002
manyy <manyy.openkt@gmail.com>
2019-09-16 01:40:37 +0800

    modify

--- libkngin/core/threadpool.h
+++ libkngin/core/threadpool.h
@@ -2,7 +2,9 @@
 #define _THREADPOLL_H_
 
 #include <vector>
+#include <atomic>
 #include "thread.h"
+#include "task_base.h"
 #include "lock.h"
 
 __NAMESPACE_BEGIN
@@ -10,6 +12,11 @@ __NAMESPACE_BEGIN
 class thread_pool {
 //join_all 无序 超时
 //cancel_all 无序 超时
+
+protected:
+    std::atomic<bool> m_running;
+
+
 };
 
 __NAMESPACE_END
