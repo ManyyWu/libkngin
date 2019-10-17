15a8dbab5bb2190afcb4af23ef337e91549e5319
manyy <manyy.openkt@gmail.com>
2019-10-17 09:39:03 +0800

    modify

--- libkngin/core/error.h
+++ libkngin/core/error.h
@@ -45,7 +45,7 @@ typedef uint32_t sys_err;
 #define __make_err_code(e) (-(e))
 
 typedef uint32_t err_code;
-
+/*
 struct __err_info {
     err_code      _code;
     const char *  _str;
@@ -57,15 +57,14 @@ extern const __err_info __err_code_entry[__ERR_CODE_NUM];
 template <class __T>
 class ret_val {
 private:
-    ret_val  () {}
+    ret_val  ()                                            {}
 
 public:
-    ret_val  (__T _val) : m_code(E_SUCCESS), m_value(_val), m_handled(true) {}
+    ret_val  (__T _val) : m_code(E_SUCCESS), m_value(_val) {}
 
-    ret_val  (const ret_val &_val) : m_code(_val.m_code), m_value(_val.m_value) {}
+    ret_val  (const ret_val &_val) : m_code(_val.m_code)   {}
 
-    ~ret_val ()
-    { if (!m_handled && m_code) log_warning("unsolved error - %s:%d", what(), m_code); }
+    ~ret_val ()                                            {}
 
 public:
     __T
@@ -76,29 +75,27 @@ public:
 
 public:
     err_code
-    code              ()               { m_handled = true; return m_code;}
+    code              ()               { return m_code;}
 
     void
-    clear             ()               { m_handled = true; }
+    clear             ()               { m_code = E_SUCCESS; }
 
     void
-    set_code          (err_code _code) { m_code = _code; m_handled = false; }
+    set_code          (err_code _code) { m_code = _code; }
 
     explicit
-    operator bool     ()               { m_handled = true; return m_code; }
+    operator bool     ()               { return m_code; }
 
     explicit
-    operator err_code ()               { m_handled = true; return m_code; }
+    operator err_code ()               { return m_code; }
 
     const char *
-    what              ()               { m_handled = true; return __err_code_entry[m_code]._str; }
+    what              ()               { return __err_code_entry[m_code]._str; }
 
 protected:
     err_code         m_code;
 
     __T              m_value;
-
-    std::atomic_bool m_handled;
 };
 #pragma pack(pop)
 
@@ -116,7 +113,7 @@ typedef class ret_val<ssize_t>   ret_ssize;
 typedef class ret_val<timestamp> ret_timestamp;
 typedef class ret_val<uintptr_t> ret_uintptr_t;
 typedef class ret_val<intptr_t>  ret_intptr_t;
-
+*/
 __NAMESPACE_END
 
 #endif /* _ERROR_H_ */
