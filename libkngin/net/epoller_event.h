d073daafe2b552691d110d7b6e83acd1e9019694
manyy <manyy.openkt@gmail.com>
2019-10-26 00:46:25 +0800

    modify

--- libkngin/net/epoller_event.h
+++ libkngin/net/epoller_event.h
@@ -17,61 +17,61 @@ public:
     typedef int                            epollfd;
 
 public:
-    epoller_event    () = delete;
+    epoller_event  () = delete;
 
     explicit
-    epoller_event    (event_loop *_loop, filefd *_s);
+    epoller_event  (event_loop *_loop, filefd *_s);
 
-    ~epoller_event   () = default;
+    ~epoller_event () = default;
 
 public:
     void
-    set_flags        (int flags) { m_flags = flags; }
+    set_flags      (int flags) { m_flags = flags; }
     int
-    flags            () const    { return m_flags; }
+    flags          () const    { return m_flags; }
     void
-    enable_once      ()          { m_flags = m_flags & ~EPOLLONESHOT; }
+    enable_once    ()          { m_flags = m_flags & ~EPOLLONESHOT; }
     void
-    disable_read     ()          { m_flags = m_flags & ~EPOLLIN; m_incb = nullptr; }
+    disable_read   ()          { m_flags = m_flags & ~EPOLLIN; m_incb = nullptr; }
     void
-    disable_write    ()          { m_flags = m_flags & ~EPOLLOUT; m_outcb = nullptr; }
+    disable_write  ()          { m_flags = m_flags & ~EPOLLOUT; m_outcb = nullptr; }
     void
-    disable_error    ()          { m_errcb = nullptr; }
+    disable_error  ()          { m_errcb = nullptr; }
     void
-    disable_ergent   ()          { m_flags = m_flags & ~EPOLLPRI; m_pricb = nullptr; }
+    disable_ergent ()          { m_flags = m_flags & ~EPOLLPRI; m_pricb = nullptr; }
     void
-    disable_once     ()          { m_flags = m_flags & ~EPOLLONESHOT; }
+    disable_once   ()          { m_flags = m_flags & ~EPOLLONESHOT; }
     void
-    disable_close    ()          { m_closecb = nullptr; }
+    disable_close  ()          { m_closecb = nullptr; }
     bool
-    pollin           () const    { return (m_flags & EPOLLIN); }
+    pollin         () const    { return (m_flags & EPOLLIN); }
     bool
-    pollout          () const    { return (m_flags & EPOLLOUT); }
+    pollout        () const    { return (m_flags & EPOLLOUT); }
     bool
-    pollpri          () const    { return (m_flags & EPOLLPRI); }
+    pollpri        () const    { return (m_flags & EPOLLPRI); }
     bool
-    pollonce         () const    { return (m_flags & EPOLLONESHOT); }
+    pollonce       () const    { return (m_flags & EPOLLONESHOT); }
     bool
-    pollhup          () const    { return (m_flags & EPOLLHUP); }
+    pollhup        () const    { return (m_flags & EPOLLHUP); }
 
     void
-    update           ();
-
-public:
-    void
-    set_read_cb      (epoller_event_cb &&_fn) { m_incb = std::move(_fn); m_flags |= EPOLLIN; }
-    void
-    set_write_cb     (epoller_event_cb &&_fn) { m_outcb = std::move(_fn); m_flags |= EPOLLOUT; }
-    void
-    set_error_cb     (epoller_event_cb &&_fn) { m_errcb = std::move(_fn); m_flags |= EPOLLERR; }
-    void
-    set_ergent_cb    (epoller_event_cb &&_fn) { m_pricb = std::move(_fn); m_flags |= EPOLLPRI; }
-    void
-    set_close_cb     (epoller_event_cb &&_fn) { m_closecb = std::move(_fn); m_flags |= EPOLLHUP; }
+    update         ();
 
 public:
     void
-    handle_events    ();
+    set_read_cb    (epoller_event_cb &&_fn) { m_incb = std::move(_fn); m_flags |= EPOLLIN; }
+    void
+    set_write_cb   (epoller_event_cb &&_fn) { m_outcb = std::move(_fn); m_flags |= EPOLLOUT; }
+    void
+    set_error_cb   (epoller_event_cb &&_fn) { m_errcb = std::move(_fn); m_flags |= EPOLLERR; }
+    void
+    set_ergent_cb  (epoller_event_cb &&_fn) { m_pricb = std::move(_fn); m_flags |= EPOLLPRI; }
+    void
+    set_close_cb   (epoller_event_cb &&_fn) { m_closecb = std::move(_fn); m_flags |= EPOLLHUP; }
+
+public:
+    void
+    handle_events  ();
 
 protected:
     event_loop *     m_loop;
