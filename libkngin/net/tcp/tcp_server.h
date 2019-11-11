3e237ec2d5399c5e22eeff4cd4c64c503baa448a
manyy <manyy.openkt@gmail.com>
2019-11-11 10:37:04 +0800

    modify

--- libkngin/net/tcp/tcp_server.h
+++ libkngin/net/tcp/tcp_server.h
@@ -23,57 +23,59 @@ public:
 
     typedef tcp_connection::close_cb               close_cb;
 
-public:
-    tcp_server        () = delete;
-
-    tcp_server        (address &&_listen_addr, const tcp_server_opts &_opts)
-
-    ~tcp_server       ();
+    typedef std::vector<tcp_connection>            tcp_connection_list;
 
 public:
+    tcp_server            () = delete;
+
+    tcp_server            (address &&_listen_addr, const tcp_server_opts &_opts)
+
+    ~tcp_server           ();
+
+public:
     void
-    run               ();
+    run                   ();
 
     void
-    stop              ();
+    stop                  ();
 
     void
-    accepting         ();
+    accepting             ();
 
 public:
     socket 
-    accept            ();
+    accept                ();
 
 public:
     bool
-    add_connection    ();
+    add_connection        ();
 
     bool
-    remove_connection ();
+    remove_connection     ();
 
 public:
     int
-    broadcast         (connection_list &_list, buffer &&_buf); // self
+    broadcast             (tcp_connection_list &_list, buffer &&_buf, bool _self = true); // self
+
+public:
+    void
+    set_new_connection_cb (new_connection_cb &&_cb) { m_new_connection_cb = std::move(_cb); }
+
+    void
+    set_read_done_cb      (read_done_cb &&_cb)      { m_read_done_cb = std::move(_cb); }
+
+    void
+    set_write_done_cb     (write_done_cb &&_cb)     { m_write_done_cb = std::move(_cb); }
+
+    void
+    set_close_cb          (close_cb &&_cb)          { m_close_cb = std::move(_cb); }
+
+    void
+    set_oob_cb            (read_oob_cb &&_cb)       { m_oob_cb = std::move(_cb); }
 
 public:
     event_loop_ptr
-    get_next_loop     ();
-
-public:
-    void
-    set_new_connection_cb (new_connection_cb &&_cb);
-
-    void
-    set_read_done_cb      (read_done_cb &&_cb);
-
-    void
-    set_write_done_cb     (write_done_cb &&_cb);
-
-    void
-    set_close_cb          (close_cb &&_cb);
-
-    void
-    set_oob_cb            (read_oob_cb &&_cb);
+    assign_thread         ()                        { return m_threadpool.next_loop(); }
 
 protected:
     void
@@ -92,25 +94,23 @@ protected:
     on_oob                (tcp_connection &_conn);
 
 protected:
-    // assign_thread
+    io_threadpool       m_threadpool;
 
-protected:
-    io_threadpool     m_threadpool;
+#ifndef NDEBUG
+    tcp_connection_list m_connections;
+#endif
 
-    // m_listener
-    // m_connections;
-    // m_max_connection
-    // m_accept_new
+    tcp_connection      m_listener;
 
-    new_connection_cb m_new_connection_cb;
+    new_connection_cb   m_new_connection_cb;
 
-    write_done_cb     m_write_done_cb;
+    write_done_cb       m_write_done_cb;
 
-    read_done_cb      m_read_done_cb;
+    read_done_cb        m_read_done_cb;
 
-    read_oob_cb       m_oob_cb;
+    read_oob_cb         m_oob_cb;
 
-    close_cb          m_close_cb;
+    close_cb            m_close_cb;
 };
 
 __NAMESPACE_END
