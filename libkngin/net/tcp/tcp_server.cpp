c32eb393496680c8947f56513ab8a0b794636a68
manyy <manyy.openkt@gmail.com>
2019-11-11 10:47:30 +0800

    modify

--- libkngin/net/tcp/tcp_server.cpp
+++ libkngin/net/tcp/tcp_server.cpp
@@ -1,3 +1,17 @@
+#include "define.h"
+#include "tcp_server.h"
+#include "common.h"
+#include "logfile.h"
+
+__NAMESPACE_BEGIN
+
+tcp_server::tcp_server (address &&_listen_addr, const tcp_server_opts &_opts)
+{
+
+}
+
+__NAMESPACE_END
+
 // signal ergent
 // adjust size of epoll_event_set
 // wait for looping
