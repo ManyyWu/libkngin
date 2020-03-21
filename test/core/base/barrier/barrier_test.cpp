5c6e209f5c50dd9d48685bd3e152a5a0a6bc2945
manyy <manyy.openkt@gmail.com>
2020-03-22 03:06:11 +0800

    modify

--- test/core/base/barrier/barrier_test.cpp
+++ test/core/base/barrier/barrier_test.cpp
@@ -8,36 +8,40 @@ int
 main () {
   k::barrier b(4);
 
-  k::thread t([&b] () -> int {
-    if (b.wait()) {
-      b.destroy();
-      fprintf(stderr, "t destoryed barrier\n");
-    }
-    return 0;
-  });
-  k::thread t1([&b] () -> int {
-    if (b.wait()) {
-      b.destroy();
-      fprintf(stderr, "t1 destoryed barrier\n");
-    }
-    return 0;
-  });
-  k::thread t2([&b] () -> int {
-    if (b.wait()) {
-      b.destroy();
-      fprintf(stderr, "t2 destoryed barrier\n");
-    }
-    return 0;
-  });
+  for (int i = 0; i < 10; ++i)
+  {
+    k::thread t([&b]() -> int {
+      if (b.wait()) {
+        b.destroy();
+        fprintf(stderr, "t destoryed barrier\n");
+      }
+      return 0;
+    });
+    k::thread t1([&b]() -> int {
+      if (b.wait()) {
+        b.destroy();
+        fprintf(stderr, "t1 destoryed barrier\n");
+      }
+      return 0;
+    });
+    k::thread t2([&b]() -> int {
+      if (b.wait()) {
+        b.destroy();
+        fprintf(stderr, "t2 destoryed barrier\n");
+      }
+      return 0;
+    });
 
-  if (b.wait()) {
-    b.destroy();
-    fprintf(stderr, "main destoryed barrier\n");
+    if (b.wait()) {
+      b.destroy();
+      fprintf(stderr, "main destoryed barrier\n");
+    }
+
+    t.join();
+    t1.join();
+    t2.join();
+    b.reinit(4);
   }
-
-  t.join();
-  t1.join();
-  t2.join();
 
   return 0;
 }
