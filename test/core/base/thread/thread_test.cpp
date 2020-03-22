a0c39867b42126559b34d124e0dc1fea955620fa
manyy <manyy.openkt@gmail.com>
2020-03-22 16:24:32 +0800

    modify

--- test/core/base/thread/thread_test.cpp
+++ test/core/base/thread/thread_test.cpp
@@ -8,7 +8,7 @@ main () {
   k::thread::thread_opt opt = {1};
 
   k::thread t([] () -> int {
-    //throw_exception("test");
+    throw_exception("test");
     return 0;
   }, "thread_test", &opt);
   t.join();
